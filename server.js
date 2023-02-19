0e404ab4618a42e51e1727623de68a9a2ee345f9
James Vong <j.vong2020@gmail.com>
2023-02-18 22:29:24 -0800

    Update server.js
    cors origin

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ const port = process.env.PORT || 8080;
 const bodyParser =require('body-parser');
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+    origin: "*"
+}));
 
 app.use('/graphs', graphs);
 

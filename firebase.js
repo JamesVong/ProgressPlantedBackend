47b8461e848e8b43b2366040b1958c6b2be0459d
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2023-02-18 16:23:04 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
 
 var admin = require("firebase-admin");
 require('dotenv').config();
-var serviceAccount;//= require("./progressplanted-firebase-adminsdk-964z8-16de8d067a.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount || 
+  credential: admin.credential.cert(
     {
-        "type":process.ENV.type,
-        "project_id":process.ENV.project_id,
-        "private_key_id":process.ENV.private_key_id,
-        "private_key":process.ENV.private_key,
-        "client_email":process.ENV.client_email,
-        "client_id":process.ENV.client_id,
-        "auth_uri":process.ENV.auth_uri,
-        "token_uri":process.ENV.token_uri,
-        "auth_provider_x509_cert_url":process.ENV.auth_provider_x509_cert_url,
-        "client_x509_cert_url":process.ENV.client_x509_cert_url
+        "type":process.env.type,
+        "project_id":process.env.project_id,
+        "private_key_id":process.env.private_key_id,
+        "private_key":process.env.private_key,
+        "client_email":process.env.client_email,
+        "client_id":process.env.client_id,
+        "auth_uri":process.env.auth_uri,
+        "token_uri":process.env.token_uri,
+        "auth_provider_x509_cert_url":process.env.auth_provider_x509_cert_url,
+        "client_x509_cert_url":process.env.client_x509_cert_url
     }),
   databaseURL: "https://progressplanted-default-rtdb.firebaseio.com"
 });

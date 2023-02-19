45bda336b3b0b47a6807b958db39b9f04156a153
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2023-02-18 16:01:47 -0800

    config

--- firebase.js
+++ firebase.js
@@ -1,10 +1,22 @@
 
 var admin = require("firebase-admin");
-
-var serviceAccount = require("./progressplanted-firebase-adminsdk-964z8-16de8d067a.json");
+require('dotenv').config();
+var serviceAccount;//= require("./progressplanted-firebase-adminsdk-964z8-16de8d067a.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount || 
+    {
+        "type":process.ENV.type,
+        "project_id":process.ENV.project_id,
+        "private_key_id":process.ENV.private_key_id,
+        "private_key":process.ENV.private_key,
+        "client_email":process.ENV.client_email,
+        "client_id":process.ENV.client_id,
+        "auth_uri":process.ENV.auth_uri,
+        "token_uri":process.ENV.token_uri,
+        "auth_provider_x509_cert_url":process.ENV.auth_provider_x509_cert_url,
+        "client_x509_cert_url":process.ENV.client_x509_cert_url
+    }),
   databaseURL: "https://progressplanted-default-rtdb.firebaseio.com"
 });
 module.exports=admin;

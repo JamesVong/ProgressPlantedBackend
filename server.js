ff0ce662b30b1662a95834c596eb28062a23c214
James Vong <j.vong2020@gmail.com>
2023-02-18 17:51:46 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const cors =require("cors");
 const app = express();
+
 // Growth Graph
 const graphs =require("./routes/growthgraph");
 const port = process.env.PORT || 8080;
